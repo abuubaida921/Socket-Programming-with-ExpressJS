41b9199c6de619c6c39da2962164cc4dc80c9a39
Abu Ubaida <ubaidanazia@gmail.com>
2024-07-30 20:18:36 +0600

    Showing new user a welcome message and update the other clients about him/her joining

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ var clients = 0;
 //Whenever someone connects this gets executed
 io.on('connection', function (socket) {
     clients++;
-    io.sockets.emit('broadcast', { description: clients + ' clients connected!' });
+    socket.emit('newclientconnect',{ description: 'Hey, welcome!'});
+    socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
     socket.on('disconnect', function () {
-        clients--;
-        io.sockets.emit('broadcast', { description: clients + ' clients connected!' });
+       clients--;
+       socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
     });
 
     //receive event from client side
